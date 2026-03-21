749742299a41597df7ff0c09b74def42ecca8f38
Igor <igorv88@gmail.com>
2026-03-21 00:31:11 -0300

    Remove BrowserRouter wrapper from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { routes } from './routes';
 import Home from './pages/Home';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {routes.map(route => (
-          <Route key={route.path} path={route.path} element={route.element} />
-        ))}
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {routes.map(route => (
+        <Route key={route.path} path={route.path} element={route.element} />
+      ))}
+    </Routes>
   );
 }
