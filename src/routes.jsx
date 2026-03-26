161a8cac10df5fc4229bd7fa19a624668be5cdb1
Igor <igorv88@gmail.com>
2026-03-26 18:06:51 -0300

    Update import path for PedCalcDrugsExtra

--- src/routes.jsx
+++ src/routes.jsx
@@ -17,7 +17,7 @@ import Politrauma from './pages/Politrauma';
 import ViaAerea from './pages/ViaAerea';
 import CicloMenstrual from './pages/CicloMenstrual';
 import TriagemNeonatal from './pages/TriagemNeonatal';
-import { EXTRA_CATEGORIES, EXTRA_DRUGS } from "./PedCalcDrugsExtra";
+import { EXTRA_CATEGORIES, EXTRA_DRUGS } from "./pages/PedCalcDrugsExtra";
 
 export const routes = [
   {
